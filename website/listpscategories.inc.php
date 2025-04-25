4b79212539eee0913c979318ff55e06cd229d6ae
Manahil Syed <syed_m8@yahoo.com>
2025-04-25 16:09:20 -0400

    public

--- website/listpscategories.inc.php
+++ website/listpscategories.inc.php
@@ -21,7 +21,6 @@
 <form name="categories" method="post">
    <select ondblclick="listbox_dblclick()" name="PScategoryID" size="20">
 <?php
-//Manahil Syed, 10/18/2024, IT-202-005, Phase 2 assignment, ms379@njit.edu
 
 //include("pscategory.php");
 $PScategories = PSCategory::getPSCategories();
